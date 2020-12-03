c87890229c71244b9b9dc65cb246727849a8585a
Veronika <veronikakuchtova95@gmail.com>
2020-12-03 11:36:02 +0100

    Milestone 2 - stars

--- js/script.js
+++ js/script.js
@@ -6,9 +6,13 @@ var app = new Vue ({
   },
   methods: {
     searchMovie: function () {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=da5b0a9b54cbfc601e8917f9f7d3843c&query=' + this.search)
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=da5b0a9b54cbfc601e8917f9f7d3843c', {
+        params: {
+          query: this.search
+        }
+      })
       .then(response => {
-        this.movies = response.data.results
+        this.movies = response.data.results;
       });
     }
   }
