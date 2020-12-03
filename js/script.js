e0e92cc066002e38d7819d2d08194e1865ac11a5
Veronika <veronikakuchtova95@gmail.com>
2020-12-03 18:03:26 +0100

    M2 series - better solution

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
+const movieApi = 'https://api.themoviedb.org/3/search/movie?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
+const tvApi = 'https://api.themoviedb.org/3/search/tv?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
+
 var app = new Vue ({
   el: '#app',
   data: {
     search: '',
-    movies: []
+    moviesTv: []
   },
   methods: {
     searchMovie: function () {
-      axios.get('https://api.themoviedb.org/3/search/multi?api_key=da5b0a9b54cbfc601e8917f9f7d3843c', {
+      let requestOne = axios.get(movieApi, {
         params: {
           query: this.search
         }
-      })
-      .then(response => {
-        this.movies = response.data.results;
       });
-    }
+      let requestTwo = axios.get(tvApi, {
+        params: {
+          query: this.search
+        }
+      });
+      axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+        let movies = responses[0].data.results;
+        let tvs = responses[1].data.results;
+        this.moviesTv = [...movies, ...tvs];
+      }));
+   }
   }
 })
