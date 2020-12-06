454d22e255208f186580192d79b099caf456eb2b
Veronika <veronikakuchtova95@gmail.com>
2020-12-06 12:58:31 +0100

    Milestone 3

--- js/script.js
+++ js/script.js
@@ -9,21 +9,27 @@ var app = new Vue ({
   },
   methods: {
     searchMovie: function () {
-      let requestOne = axios.get(movieApi, {
-        params: {
-          query: this.search
-        }
-      });
-      let requestTwo = axios.get(tvApi, {
-        params: {
-          query: this.search
-        }
-      });
+      // let requestOne = axios.get(movieApi, {
+      //   params: {
+      //     query: this.search
+      //   }
+      // });
+      // let requestTwo = axios.get(tvApi, {
+      //   params: {
+      //     query: this.search
+      //   }
+      // });
+      let callApi = api => axios.get(api, {params: {query: this.search}});
+
+      let requestOne = callApi(movieApi);
+      let requestTwo = callApi(tvApi);
+
       axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
         let movies = responses[0].data.results;
         let tvs = responses[1].data.results;
         this.moviesTv = [...movies, ...tvs];
       }));
+
    }
   }
 })
