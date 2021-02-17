5c5ee4a6e04a295aa45992f8eb373b9343790301
Veronika <veronikakuchtova95@gmail.com>
2021-02-17 22:59:28 +0100

    Added movies in Home

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const movieApi = 'https://api.themoviedb.org/3/search/movie?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
 const tvApi = 'https://api.themoviedb.org/3/search/tv?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
+// const tendingMovieApi ='https://api.themoviedb.org/3/trending/movie/week?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
 
 var app = new Vue ({
   el: '#app',
@@ -7,18 +8,17 @@ var app = new Vue ({
     search: '',
     moviesTv: []
   },
+  // Popular shows
+  mounted : function () {
+    axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=da5b0a9b54cbfc601e8917f9f7d3843c')
+
+    .then(response => {
+      this.moviesTv = response.data.results;
+    });
+  },
+  // Search Results
   methods: {
     searchMovie: function () {
-      // let requestOne = axios.get(movieApi, {
-      //   params: {
-      //     query: this.search
-      //   }
-      // });
-      // let requestTwo = axios.get(tvApi, {
-      //   params: {
-      //     query: this.search
-      //   }
-      // });
       let callApi = api => axios.get(api, {params: {query: this.search}});
 
       let requestOne = callApi(movieApi);
