cce4e729dcfdb938e8668c403283a16b9295789c
Veronika <veronikakuchtova95@gmail.com>
2021-02-20 23:13:08 +0100

    Added popular tv shows in home

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const movieApi = 'https://api.themoviedb.org/3/search/movie?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
 const tvApi = 'https://api.themoviedb.org/3/search/tv?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
-// const tendingMovieApi ='https://api.themoviedb.org/3/trending/movie/week?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
+const trendingMovieApi ='https://api.themoviedb.org/3/trending/movie/week?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
+const trendingTvApi = 'https://api.themoviedb.org/3/trending/tv/week?api_key=da5b0a9b54cbfc601e8917f9f7d3843c';
 
 var app = new Vue ({
   el: '#app',
@@ -10,11 +11,18 @@ var app = new Vue ({
   },
   // Popular shows
   mounted : function () {
-    axios.get('https://api.themoviedb.org/3/trending/movie/week?api_key=da5b0a9b54cbfc601e8917f9f7d3843c')
+    let requestOne = axios.get(trendingMovieApi);
+    let requestTwo = axios.get(trendingTvApi);
 
-    .then(response => {
-      this.moviesTv = response.data.results;
-    });
+    axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+      let movies = responses[0].data.results;
+      let tvs = responses[1].data.results;
+      this.moviesTv = [...movies, ...tvs];
+    }));
+
+    // .then(response => {
+    //   this.moviesTv = response.data.results;
+    // });
   },
   // Search Results
   methods: {
